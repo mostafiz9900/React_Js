9231ce3ebd182a874ab24d8d9347e686b78d08d3
DESKTOP-PPM840I\AI Software <codergitbd@gmail.com>
2020-09-03 13:38:00 +0600

    table
    table

--- my-route2/src/App.js
+++ my-route2/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import styles from './Mycss.module.css';
+// import styles from './Mycss.module.css';
+import './My.scss'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="textStyle">Test app csss</h1>
-      <h1 className={styles.textStyle2}>Test app csss</h1>
+      {/* <h1 className="textStyle">Test app csss</h1> */}
+      {/* <h1 className={styles.textStyle2}>Test app csss</h1> */}
+      <h1 className="TextStyle">Mostafizur </h1>
  
     </div>
   );
